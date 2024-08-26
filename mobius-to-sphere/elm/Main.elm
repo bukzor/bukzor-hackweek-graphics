869940c070f102eeb89102dbb23cecc54a926046
Buck Evan <buck.evan@sentry.io>
2024-08-26 14:16:16 -0500

    elm webgl: successfully render a magenta triangle

--- mobius-to-sphere/elm/Main.elm
+++ mobius-to-sphere/elm/Main.elm
@@ -1,38 +1,74 @@
-module Main exposing (view)
+module Main exposing (main)
 
 import Browser
-import Html exposing (Html, button, div, text)
-import Html.Events exposing (onClick)
+import Html exposing (Html)
+import Math.Vector2 exposing (Vec2, vec2)
+import Math.Vector3 exposing (Vec3, vec3)
+import WebGL
 
 
-main : Program () Model Msg
-main =
-    Browser.sandbox { init = 30, update = update, view = view }
+type alias Vertex =
+    { position : Vec2
+    , color : Vec3
+    }
 
 
-type Msg
-    = Increment
-    | Decrement
+vertexShader : WebGL.Shader Vertex Model {}
+vertexShader =
+    [glsl|
+        attribute vec2 position;
+        attribute vec3 color;
+        uniform vec3 vColor;
+        void main () {
+            gl_Position = vec4(position, 0.0, 1.0);
+        }
+    |]
 
 
-update : Msg -> number -> number
-update msg model =
-    case msg of
-        Increment ->
-            model + 1
+fragmentShader : WebGL.Shader {} Model {}
+fragmentShader =
+    [glsl|
+        precision highp float;
+        uniform vec3 vColor;
+        void main () {
+            gl_FragColor = vec4(vColor, 1.0);
+        }
+    |]
 
-        Decrement ->
-            model - 1
+
+vertices : List Vertex
+vertices =
+    -- the vertices of the triangle
+    [ { position = vec2 0 0.5, color = vec3 1 0 0 }
+    , { position = vec2 0.5 -0.5, color = vec3 0 1 0 }
+    , { position = vec2 -0.5 -0.5, color = vec3 0 0 1 }
+    ]
+
+
+buffer : WebGL.Mesh Vertex
+buffer =
+    -- convert vertices to WebGL buffers
+    WebGL.lineLoop vertices
 
 
 type alias Model =
-    Int
+    -- Define the model
+    { vColor : Vec3 }
 
 
-view : Model -> Html Msg
+init : Model
+init =
+    -- the initial model
+    { vColor = vec3 1 0 1 }
+
+
+view : Model -> Html msg
 view model =
-    div []
-        [ button [ onClick Decrement ] [ text "-" ]
-        , div [] [ text (String.fromInt model) ]
-        , button [ onClick Increment ] [ text "+" ]
-        ]
+    -- Define the view function
+    WebGL.toHtml [] [ WebGL.entity vertexShader fragmentShader buffer model ]
+
+
+main : Program () Model msg
+main =
+    -- the entry point
+    Browser.sandbox { init = init, update = \_ model -> model, view = view }
