56cf26017b9672ad80e03fa1bc93f4b284f0f324
Buck Evan <buck.evan@sentry.io>
2024-08-27 16:10:11 -0500

    rust: modularized

---
+++ mobius-to-sphere/rust/src/wgpu.rs
@@ -0,0 +1,9 @@
+//#[derive(Default)]
+//struct WgpuApplication<'a> {
+//    surface: wgpu::Surface<'a>,
+//    device: wgpu::Device,
+//    queue: wgpu::Queue,
+//    config: wgpu::SurfaceConfiguration,
+//    size: winit::dpi::PhysicalSize<u32>,
+//    window: &'a Window,
+//}
